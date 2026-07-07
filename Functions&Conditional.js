4d171ec24d3025ac7e2af7555707c715679d1141
Hardik <patilhardikmaheshbhai@gmail.com>
2026-07-07 17:26:35 +0530

    Added new file

---
+++ Functions&Conditional.js
@@ -0,0 +1,14 @@
+function add(a,b){
+   return a+b;
+}
+
+console.log(add(5,3));
+
+
+//conditional statements
+let age=21;
+if(age >=18){
+   console.log("Adult");
+}else{
+   console.log("Minor");
+}
